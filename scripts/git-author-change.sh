02f05be8242a7cab1661e2dc89f1f43864622bda
Brian H. Buchholz <4773480+bhb603@users.noreply.github.com>
2019-02-21 12:50:54 -0500

    update git-author-change

--- scripts/git-author-change.sh
+++ scripts/git-author-change.sh
@@ -4,28 +4,33 @@
 # Based on https://help.github.com/articles/changing-author-info/
 #
 
-repos="\
+REPOS="\
 foo
 bar
+baz
 "
 
-dir=$(pwd)
+OLD_EMAIL=""
+CORRECT_NAME="Brian H. Buchholz"
+
+CORRECT_EMAIL="4773480+bhb603@users.noreply.github.com"
+HOST=git@github.com:bhb603
+
+CORRECT_EMAIL="3230751-bhb603@users.noreply.gitlab.com"
+HOST=git@gitlab.com:bhb603
+
+DIR=$(pwd)
 
 change_author_info()
 {
   git filter-branch --env-filter '
 
-  OLD_EMAIL="foo@bar.com"
-  OLD_EMAIL_2="bar@baz.com"
-  CORRECT_NAME="Foo J. Bar"
-  CORRECT_EMAIL="anon@users.noreply.github.com"
-
-  if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ] || [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL_2" ]
+  if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
   then
       export GIT_COMMITTER_NAME="$CORRECT_NAME"
       export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
   fi
-  if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ] || [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL_2" ]
+  if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
   then
       export GIT_AUTHOR_NAME="$CORRECT_NAME"
       export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
@@ -34,12 +39,12 @@ change_author_info()
 }
 
 
-for repo in $repos; do
+for repo in $REPOS; do
   echo "Cloning $repo"
-  git clone --bare "git@github.com:bhb603/$repo.git"
+  git clone --bare "$HOST/$repo.git"
   cd "$repo.git"
   change_author_info
   git push --force --tags origin 'refs/heads/*'
-  cd "$dir"
+  cd "$DIR"
   rm -rf "$repo.git"
 done
